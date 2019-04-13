56dc2ed653e081f66cf27e9ac149336b1815d258
matthewparnham <parnham@chapman.edu>
2019-04-13 14:00:21 -0700

    Added Client Credentials and little program to get a user's playlists

---
+++ main.py
@@ -0,0 +1,18 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Get the username from input
+username = input("Spotify Username: ")
+
+client_credentials_manager = SpotifyClientCredentials(client_id='d1eebb993a9849288e221d27b95158f5', client_secret='0115f96419b848c0a5e2e28d1257618e')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlists = sp.user_playlists(username)
+
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
