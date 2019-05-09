dea46a5288ab7c3a3d4141ce54107655849d52b5
arigl <42983251+arigl@users.noreply.github.com>
2019-05-09 15:31:55 -0700

    Create webtest2.py

---
+++ HTML&CSS/cgi-bin/webtest2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Import modules for CGI handling
+import cgi, cgitb
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields
+first_name = form.getvalue('username2')
+
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>Python Program</title>"
+print "</head>"
+print "<body>"
+print "<h2>Hello %s</h2>" % (first_name)
+print "<h2>This is a python program</h2>"
+print "</body>"
+print "</html>"
